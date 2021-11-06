548f397198850b4b198e9814ed880f2e92aa9121
MOHAMMED HAZIQ <haziqinfinity@gmail.com>
2021-11-06 12:14:01 +0530

    Add files via upload

---
+++ Inbuild_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void display(int a[], int n)
+{
+
+  cout << "[";
+  for (int i = 0; i < n; i++)
+  {
+    if (i < n - 1)
+    {
+      cout << a[i] << ",";
+    }
+    else
+    {
+      cout << a[i] << "]";
+    }
+  }
+}
+int main(int argc, char const *argv[])
+{
+  int n;
+  cout << "Enter the size of array:";
+  cin >> n;
+  int a[n];
+  cout << "\nInput array elements:";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  cout << "\nYour arrray before sorting is:" << endl;
+  display(a, n);
+  sort(a, a + n);
+  cout << "\nYour arrray after sorting is:" << endl;
+  display(a, n);
+  return 0;
+}
